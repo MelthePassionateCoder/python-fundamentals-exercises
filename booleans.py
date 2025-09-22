7bce335e6842b0ec71ab115f6b22b973760b378d
Mel-liza31 <limbagomelliza31@gmail.com>
2025-09-22 23:00:58 +0800

    feat:simple age checker

---
+++ booleans.py
@@ -0,0 +1,8 @@
+#Create a simple age checker
+
+age = input("Enter your age: ")
+
+if int(age) >= 18:
+    print("\nYou're ready to adulthood!!!!")
+else: 
+    print("\nEnjoy your childhood/adolescence stage!!!!")
