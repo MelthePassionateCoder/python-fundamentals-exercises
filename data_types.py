3bf820f847ee798ffa0c04456b955876c2b465fc
Mel-liza31 <limbagomelliza31@gmail.com>
2025-09-22 22:43:25 +0800

    feat: sho at least 4 different data types and print their types

---
+++ data_types.py
@@ -0,0 +1,11 @@
+#Show at least 4 different data types (int, float, str, list) and print their types.
+
+a = 3
+b = 3.5
+c = "I am going to be the best programmer"
+d = ["I", "am", "a", "list"]
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
