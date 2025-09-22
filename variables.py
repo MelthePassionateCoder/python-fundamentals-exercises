329bc73bad7fd620f05cd81c636cf9270e146046
Mel-liza31 <limbagomelliza31@gmail.com>
2025-09-22 22:27:52 +0800

    feat: add variable practice with name, age, and favorite place

---
+++ variables.py
@@ -0,0 +1,7 @@
+#A program to store the name, age, and favorite place and display all.
+
+name = "Liza"
+age = 25
+favoritePlace = "Sagada"
+
+print(f"My name is {name} and I am {age} years old. My favorite place is {favoritePlace}")
